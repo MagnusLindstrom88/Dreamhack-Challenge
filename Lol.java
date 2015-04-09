b01eb58cb51873cf8487815255fea63fa27cec1c
Firstname Surname <sebwar01@hotmail.com>
2015-04-09 02:13:27 +0200

    oosdads sdasd asd

--- Lol.java
+++ Lol.java
@@ -1,5 +1,5 @@
 public class Lol {
-public static void main(String[] args) {
+public static void main(String[] args) {  //This comment was added in my local project folder.
 System.out.println("lol");}  //This comment was added on GitHub.
 }
 //Hey there
