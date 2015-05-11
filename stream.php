e43d72c1d812e120d7695783eaf85780ba1bf262
irothag <robban_rulz@hotmail.com>
2015-05-11 16:57:24 +0200

    Stream Sidan

--- stream.php
+++ stream.php
@@ -23,7 +23,12 @@
 									<div class="col-md-8">
 										 <ul id="stream-list" class="list-group">
 											<li class="list-group-item">
-										<p class="list-group-item-text">Stream här</p>
+										<p class="list-group-item-text">
+										
+										<iframe src="http://www.twitch.tv/asiagodtonegg3be0/embed" frameborder="0" scrolling="no" height="378" width="620"></iframe>
+										<a href="http://www.twitch.tv/asiagodtonegg3be0?tt_medium=live_embed&tt_content=text_link" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px;text-decoration:underline;">Watch live video from asiagodtonegg3be0 on www.twitch.tv</a>
+										
+										</p>
 									</li>
 								</li>
 							</ul>
@@ -31,7 +36,11 @@
 							<div class="col-md-4">
 							<ul id="stream-list" class="list-group">
 								<li class="list-group-item">
-									<p class="list-group-item-text">Chatten</p>
+									<p class="list-group-item-text">
+									
+									<iframe src="http://www.twitch.tv/asiagodtonegg3be0/chat?popout=" frameborder="0" scrolling="no" height="500" width="350"></iframe>
+									
+									</p>
 									</li>
 								</li>
 							</ul>
