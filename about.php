088f80ba0084e1345b5d7c2a5a692c583a508ee9
unknown <robban_rulz@hotmail.com>
2015-05-11 18:50:18 +0200

    fixat about och jimmys CSS

---
+++ about.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Prototyp</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    
+    <link rel="stylesheet" href="CSS.css">
+        <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/shift.css" rel="stylesheet">
+</head>
+<body>
+    <!--Header-->
+    <header class="container">
+        <img src="images/logo.png" alt="Logo" style="width:300px"/>
+    </header>
+    
+    <!--Navigation bar-->
+    <nav id="navbar" class="navbar navbar-default">
+        <div class="container">
+            <!--The toggle button for collapsed menu.-->
+            <button class="navbar-toggle" data-toggle="collapse" data-target="#menuFields">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!--The brand on the leftmost side.-->
+            <div class="navbar-header">
+                <a class="navbar-brand">Navigation</a>
+            </div>
+
+            <!--The menu fields. Will be collapsed on small screens.-->
+            <div class="collapse navbar-collapse" id="menuFields">
+                <!--Fields on the left.-->
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Games<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="csgo.php">Counter-Strike: Global Offensive</a></li>
+                            <li><a href="dota2.php">Dota 2</a></li>
+                            <li><a href="StarcraftII">Starcraft II</a></li>
+                        </ul>
+                    </li>
+                    
+                    <li><a href="#">About</a></li>
+
+                </ul>
+                <!--Fields on the right.-->
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Register</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    </div>
+
+<div class="container">
+    <div class="row">
+    <div class="col-md-16"><h1 style="color: white; margin-bottom:20px;" class="text-center">About Us & Games</h1></div>
+  </div>
+
+<div class="container">
+  <div class="container">
+    <div class="footer-images">
+     <img src="images/csgologo.jpg">
+      <img src="images/dota2.jpg">
+      <img src="images/starcraft2thumbnail.jpg">
+      <div class="container">
+        <div class="copyright">
+        </div>
+
+<div class="block">
+ <div style ="text-align:center">
+        <div style="display: inline-block; margin-right: 5px;">
+<div class="promo-3s mr10"><img src="ppp.png" width="65" height="65" />
+<h3>Best Value Odds</h3>
+<p>We offer the best free online odds for free</p>
+</div>
+</div>
+<div style="display: inline-block">
+<div class="promo-3s mr10"><img src="www.png"
+    alt="promotions-winnerswelcome-icon.png" width="65" height="65" />
+
+<h3>Winners Are Welcome</h3>
+<p>You got nothing to lose, just start betting and win!</p>
+      </div>
+</div>
+
+<div style="display: inline-block">
+<div class="promo-3s"><img src="star.png"
+ width="65" height="65" />
+<h3>We are Dreamhack Challenge Bettingsite, we offer livestreams, betting to win fine prices</h3>
+<p> Hopefully many of you will join our community to watch livestream, and also to have fun</p>
+<a href="" class="btn btn-primary">Learn More</a>
+</div>
+    </div>
+  </div>
+
+
+</div>
+
+<div class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+<div class="container">
+<div class="navbar-text-pull-left">
+<p> Dreamhack Challenge 2015 </p>
+</div>
