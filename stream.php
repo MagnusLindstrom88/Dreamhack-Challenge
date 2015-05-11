11fe7eb5184963a55704643ea04f96b20a52c31b
irothag <robban_rulz@hotmail.com>
2015-05-11 17:21:20 +0200

    Stream Sidan

--- stream.php
+++ stream.php
@@ -9,6 +9,28 @@
             border:1px solid #676461;
         }
     </style>
+	<script src="//ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script>
+      $(function () {
+        window.onPlayerEvent = function (data) {
+          data.forEach(function(event) {
+            if (event.event == "playerInit") {
+              var player = $("#twitch_embed_player")[0];
+              player.playVideo();
+              player.mute();
+            }
+          });
+        }
+
+        swfobject.embedSWF("//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf", "twitch_embed_player", "640", "400", "11", null,
+          { "eventsCallback":"onPlayerEvent",
+            "embed":1,
+            "channel":"day9tv",
+            "auto_play":"true"},
+          { "allowScriptAccess":"always",
+            "allowFullScreen":"true"});
+      });
 </head>
 <body>
     <div id="wrapper">
