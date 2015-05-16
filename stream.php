7037437332022d320e2925491677bf380a057e6c
irothag <robban_rulz@hotmail.com>
2015-05-16 20:50:03 +0200

    Mher

--- stream.php
+++ stream.php
@@ -80,7 +80,7 @@
 								<li class="list-group-item">
 									<p class="list-group-item-text">
 									<div class="embed-container">
-									<iframe src="chat.php" width="450" height="450" frameborder="0" scrolling="yes"></iframe>
+									<iframe src="chat.php" height="450" frameborder="0" scrolling="yes"></iframe>
 									</div>
 									</p>
 									</li>
