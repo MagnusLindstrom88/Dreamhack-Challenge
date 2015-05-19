8464f254936ddaa71200d5213a8a202ed333cc76
Firstname Surname <sebwar01@hotmail.com>
2015-05-19 20:18:00 +0200

    Bekräftelse via email tillagt.

--- scripts/login&registration/check_data_exists.php
+++ scripts/login&registration/check_data_exists.php
@@ -1,22 +1,31 @@
 <?php
-/* Contains code to check if provided data is in the database. Used when logging in to check if the email and password exists.
- * Used when registering to check if the username and email exists. */
+/* Contains code to check if certain data is in the database. Used when logging in to check if the email and password exists.
+ * Also used by login to check if the account is verified. Used when registering to check if the username and email exists. */
 require_once '../init.php';
 
 //Code used for login validation.
 if($_POST['from'] === "login") {
+    $returnString = "";
     $emailOrUsername = $_POST['emailorusername'];
     $ps1 = $db->prepare("SELECT * FROM users WHERE email=?");
     $ps2 = $db->prepare("SELECT * FROM users WHERE username=?");
     $ps1->execute(array($emailOrUsername));
     $ps2->execute(array($emailOrUsername));
     if($ps1->rowCount() > 0 || $ps2->rowCount() > 0) {
-        echo "email or username found";
+        $returnString .= "email or username found";
         $password = $_POST['password'];
         if($ps1->rowCount() > 0) $hashed_password = $ps1->fetchAll(PDO::FETCH_ASSOC)[0]['password'];
         else $hashed_password = $ps2->fetchAll(PDO::FETCH_ASSOC)[0]['password'];
-        if($hashed_password === crypt($password, $hashed_password)) echo "password found";
+        if($hashed_password === crypt($password, $hashed_password)) $returnString .= "password found";
     }
+    //Check if the account has been verified through email.
+    if(strpos($returnString, "password found") !== false) {
+        $verificationKey = $db->query("SELECT verification_key FROM users WHERE password='$hashed_password'");
+        $verificationKey = $verificationKey->fetchAll(PDO::FETCH_NUM)[0][0];
+        if(!empty($verificationKey)) $returnString .= "not verified";
+    }
+    
+    echo $returnString;
 }
 
 //Code used for registration validation.
--- scripts/login&registration/register.php
+++ scripts/login&registration/register.php
@@ -23,17 +23,31 @@ if(isset($_POST['email'])) {
     
     //If the verification of the captcha was successful.
     if($result['success']) {
+        //Send verification email
+        $verificationKey = generateSalt(10);
+        $subject = "DreamHack Challenge - Email Confirmation";
+        $message = "Hello," . "\n" . "\n" . "Please confirm your email by clicking on the link below: " . "\n"
+                    //. "https://pvt.dsv.su.se/Group10/verifyaccount.php?key=" . $verificationKey
+                    . "http://localhost/dreamhack-challenge/verifyaccount.php?key=3D". $verificationKey
+                    . "\n" . "\n" . "This is an automated message, please do not respond to it.";
+        $headers = 'From: sebwar01@hotmail.com' . "\n" .
+        'Reply-To: sebwar01@hotmail.com' . "\n" .
+        'Content-Type: text/plain; charset=UTF-8' . "\n" .
+        'MIME-Version: 1.0' . "\n" .
+        'Content-Transfer-Encoding: quoted-printable' . "\n";
+        mail($email, $subject, $message, $headers);
+        
         //Insert into database with a prepared statement in order eliminate any risk of SQL injection.
-        $ps = $db->prepare('INSERT INTO users(username, email, password) VALUES (?, ?, ?)');
+        $ps = $db->prepare('INSERT INTO users(username, email, password, verification_key) VALUES (?, ?, ?, ?)');
         //The default hashing algorithm uses a 2-character salt. There's a better alternative to crypt() but the server is running an old version of PHP that dosn't support it.
-        $ps->execute(array($username, $email, crypt($password, generateSalt(2))));
-        echo "Registration successful.";
+        $ps->execute(array($username, $email, crypt($password, generateSalt(2)), $verificationKey));
+        echo "Registration successful. A confirmation message has been sent to your email.";
     }
     else echo "Registration failed.";
 }
 
 function generateSalt($max = 15) {
-        $characterList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*?";
+        $characterList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%*?";
         $i = 0;
         $salt = "";
         while ($i < $max) {
