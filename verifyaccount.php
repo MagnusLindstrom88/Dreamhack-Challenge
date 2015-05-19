4225001115e0c0e549a4a097ded9940c66450e76
Firstname Surname <sebwar01@hotmail.com>
2015-05-19 20:19:12 +0200

    en fil

---
+++ verifyaccount.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'scripts/init.php';
+$ps = $db->prepare("UPDATE users SET verification_key = null WHERE verification_key = ?");
+$ps->execute(array($_GET['key']));
+
+?>
+<script>
+    alert("Verification successful.");
+    location = "index.php";
+</script>
