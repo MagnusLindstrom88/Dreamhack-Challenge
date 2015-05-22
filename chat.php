3b65c6a40d2dc5c4e85ec03b360addedfff3f27d
irothag <robban_rulz@hotmail.com>
2015-05-22 16:31:00 +0200

    Mher

--- chat.php
+++ chat.php
@@ -1,5 +1,37 @@
 <?php
 
-echo "test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>test";
+$servername = "mysql.dsv.su.se";
+$username = "sewa2700";
+$password = "eequishusaiz";
+$dbname = "sewa2700";
 
-?>
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT users.id, users.username, users.profilepic, comments.userid, comments.text
+	FROM users, comments 
+	WHERE users.id = comments.userid";
+	
+$result = $conn->query($sql);
+		
+
+if ($result->num_rows > 0) {
+    // output data of each row
+	echo '<table width="100%" border="1" cellpadding="0" cellspacing="0">';
+    while($row = $result->fetch_assoc()) {
+	
+		echo '<tr>
+			<th width="33%" align="left" height="20" bgcolor="#66CC00" scope="col"><a href="#">'. $row["users.username"] .'</a></th>	
+			<th width="66%" align="left" height="20" bgcolor="#66CC00" scope="col">'. $row["comments.text"] .'</th>		
+			</tr>';
+	}
+	echo '</table>';	
+} else {
+    echo "0 results";
+}
+$conn->close();
+?> 
