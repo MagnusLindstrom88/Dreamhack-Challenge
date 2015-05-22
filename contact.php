80c90ab3d42de35645b20e80a432c8deacfa6de1
bryo1337 <simon_Palmqvist@hotmail.com>
2015-05-22 13:38:32 +0200

    contact form

--- contact.php
+++ contact.php
@@ -36,6 +36,7 @@
         }
         
         if($error == 1){
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
           <script type="text/javascript"> $('#formModal').modal('show');</script>
         }
         else{
