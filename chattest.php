0c5f651d36123f44c05e71b128bba9eb79e47f2e
Sebwar <sebwar01@hotmail.com>
2015-05-24 21:44:49 +0200

    Update chattest.php

--- chattest.php
+++ chattest.php
@@ -27,7 +27,7 @@
             <div class="row">
                 <div class="col-md-8">
                     <div class="videoWrapper">
-                        <iframe src="http://www.twitch.tv/imaqtpie/embed" frameborder="0" scrolling="no"></iframe>
+                        <iframe src="http://www.twitch.tv/imaqtpie/embed" frameborder="0" scrolling="no" height="378" width="620"></iframe><a href="http://www.twitch.tv/imaqtpie?tt_medium=live_embed&tt_content=text_link" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px;text-decoration:underline;">Watch live video from imaqtpie on www.twitch.tv</a>
                     </div>
                 </div>
                 <div class="col-md-4" style="height:300px;background-color:white;">
