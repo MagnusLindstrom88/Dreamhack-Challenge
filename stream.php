7b8b669e200ed62ae0d8a1db547c850feb6911e3
Firstname Surname <sebwar01@hotmail.com>
2015-05-24 22:12:49 +0200

    Mer test

--- stream.php
+++ stream.php
@@ -3,33 +3,19 @@
 <head>
     <?php require_once 'template/head.php'; ?>
     <style>
-        #stream-heading {margin-bottom: 20px;}
-        #stream-list li {
-            background-color: #242424;
-            border:1px solid #676461;
-	    margin-top: 0px;
-	    margin-bottom: 0px;
-	    margin-right: 0px;
-	    margin-left: 0px;
+        .videoWrapper {
+            position: relative;
+            padding-bottom: 56.25%; /* 16:9 */
+            padding-top: 25px;
+            height: 0;
         }
-	
-	.embed-container {
-	position: relative;
-	padding-bottom: 56.25%; /* 16/9 ratio */
-	padding-top: 30px; /* IE6 workaround*/
-	height: 0;
-	overflow: hidden;
-	}
-	
-	.embed-container iframe,
-	.embed-container object,
-	.embed-container embed {
-	    position: absolute;
-	    top: 0;
-	    left: 0;
-	    width: 100%;
-	    height: 100%;
-	}	
+        .videoWrapper iframe {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }	
     </style>
 </head>
 <body>
@@ -43,14 +29,8 @@
 			<ul id="stream-list" class="list-group">
 			    <li class="list-group-item">
 				<p class="list-group-item-text">
-				<div class="embed-container" id="embed-container">
-				    <script>
-					if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-						document.getElementById('embed-container').innerHTML = '<iframe src="http://www.twitch.tv/imaqtpie/embed" frameborder="0" scrolling="no"></iframe> ';
-					} else {
-						document.getElementById('embed-container').innerHTML = '<object bgcolor="#242424" data="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" height="450" type="application/x-shockwave-flash" width="720"> <param name="allowFullScreen" value="true" /> <param name="allowNetworking" value="all" /> <param name="allowScriptAccess" value="always" /> <param name="movie" value="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" /> <param name="flashvars" value="channel=imaqtpie&auto_play=true&start_volume=25" /> </object>'; 
-					}
-				    </script>
+				<div class="videoWrapper">
+				    <iframe src="http://www.twitch.tv/imaqtpie/embed" frameborder="0" scrolling="no"></iframe>
 				</div>
 				</p>
 			    </li>
@@ -67,6 +47,8 @@
 			    </li>
 			</ul>
 		    </div>
+		</div>
+		<div class="row">
 		    <div class="col-md-12">
 			<ul id="stream-list" class="list-group">
 			    <li class="list-group-item">
