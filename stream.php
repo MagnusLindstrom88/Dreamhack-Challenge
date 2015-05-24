12d67464026b48539822a23c44fed73028a34f0d
Sebwar <sebwar01@hotmail.com>
2015-05-24 23:41:07 +0200

    Update stream.php

--- stream.php
+++ stream.php
@@ -27,7 +27,7 @@
 		<div class="row">
 		    <div class="col-md-8" id="hej" style="border: 3px solid red;">
 		    	<div class="videoWrapper">
-		    		<iframe src="http://www.twitch.tv/imaqtpie/embed" frameborder="0" scrolling="no"></iframe>	
+		    		<iframe src="https://www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf?channel=imaqtpie" frameborder="0" scrolling="no"></iframe>	
 		    	</div>
 
 		    				
@@ -71,42 +71,9 @@
     </div>
     
     <script>
-    /*
-	// Find all YouTube videos
-	var $allVideos = $("object"),
-	
-	// The element that is fluid width
-	$fluidEl = $("#hej");
-	
-	// Figure out and save aspect ratio for each video
-	$allVideos.each(function() {
-	
-	$(this)
-	    .attr('data-aspectRatio', this.height / this.width)
-	
-	    // and remove the hard coded width/height
-	    .removeAttr('height')
-	    .removeAttr('width');
-	
-	});
-	
-	// When the window is resized
-	$(window).resize(function() {
-	
-	  var newWidth = $fluidEl.width();
-	
-	  // Resize all videos according to their own aspect ratio
-	  $allVideos.each(function() {
-	
-	    var $el = $(this);
-	    $el
-	    .width(newWidth)
-	    .height(newWidth * $el.attr('data-aspectRatio'));
-	
-	  });
-	
-	// Kick off one resize to fix all videos on page load
-	}).resize();*/
+    	if (/iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+    		$("iframe")[0].src = "http://www.twitch.tv/imaqtpie/embed";
+    	}
     </script>
 </body>
 </html>
