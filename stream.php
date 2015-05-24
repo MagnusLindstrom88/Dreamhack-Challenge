50cbda7b07945398830537c27483f5d5cbded4c5
Sebwar <sebwar01@hotmail.com>
2015-05-24 23:33:29 +0200

    Update stream.php

--- stream.php
+++ stream.php
@@ -3,19 +3,7 @@
 <head>
     <?php require_once 'template/head.php'; ?>
     <style>
-	.videoWrapper {
-	position: relative;
-	padding-bottom: 56.25%; /* 16:9 */
-	padding-top: 25px;
-	height: 0;
-}
-.videoWrapper iframe {
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-}
+
     </style>
 </head>
 <body>
