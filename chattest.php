5ca861f4b272382824c0df5ec62d715761ea8fb7
Sebwar <sebwar01@hotmail.com>
2015-05-24 22:30:51 +0200

    Update chattest.php

--- chattest.php
+++ chattest.php
@@ -3,16 +3,39 @@
 <head>
     <?php require_once 'template/head.php'; ?>
     <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/shift.css" rel="stylesheet">
-
+    <style>
+        .videoWrapper {
+            position: relative;
+            padding-bottom: 56.25%; /* 16:9 */
+            padding-top: 25px;
+            height: 0;
+        }
+        .videoWrapper iframe {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }
+    </style>
 </head>
 <body>
-
-
-                 <div id="hej"></div>
-
-    <script>
-        document.getElementById('hej').innerHTML = '<iframe src="http://www.twitch.tv/imaqtpie/embed" frameborder="0" scrolling="no"></iframe> ';
-    </script>
+    <div id="wrapper">
+        <?php require_once 'template/header&navbar.php'; ?>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="videoWrapper">
+                        <iframe src="https://www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf?channel=imaqtpie" frameborder="0" scrolling="no"></iframe>
+                    </div>
+                </div>
+                <div class="col-md-4" style="height:300px;background-color:white;">
+                    
+                </div>
+            </div>
+        </div>
+    
         <?php require_once 'template/footer.php'; ?>
     </div>
 </body>
