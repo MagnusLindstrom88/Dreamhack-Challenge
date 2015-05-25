15ba4bea9aca9b08c1478334b4be51a047bb437d
Robert Norberg <robban_rulez@hotmail.com>
2015-05-25 15:31:07 +0200

    Mher

--- chat.php
+++ chat.php
@@ -25,7 +25,7 @@ if (isset($_POST['comment'])) {
 	
 		$result = $conn->query($sql);
 		
-//Form för att posta nya kommentarer
+//Form för att posta nya kommentarers
 
 if (isset($_SESSION['id'])) {
 	echo '<form action="" method="post">
