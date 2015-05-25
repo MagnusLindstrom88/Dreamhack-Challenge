a25533e02e45f339bf9a28df375aa1135a6fd4c5
Robert Norberg <robban_rulez@hotmail.com>
2015-05-25 15:40:02 +0200

    Mher

--- about.php
+++ about.php
@@ -1,130 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<<<<<<< HEAD
-    <title>aboutus</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-        
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    
-    <link rel="stylesheet" href="CSS.css">
-        <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/shift.css" rel="stylesheet">
+    <?php require_once 'template/head.php'; ?>
+    <style>
+        #faq-heading {margin-bottom: 20px;}
+        #faq-list li {
+            background-color: #242424;
+            border:1px solid #676461;
+        }
+    </style>
 </head>
 <body>
-    <!--Header-->
-    <header class="container">
-        <img src="images/Logo.png" alt="Logo" style="width:300px"/>
-    </header>
-    
-    <!--Navigation bar-->
-    <nav id="navbar" class="navbar navbar-default">
-        <div class="container">
-            <!--The toggle button for collapsed menu.-->
-            <button class="navbar-toggle" data-toggle="collapse" data-target="#menuFields">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <!--The brand on the leftmost side.-->
-            <div class="navbar-header">
-                <a class="navbar-brand">Navigation</a>
-            </div>
-
-            <!--The menu fields. Will be collapsed on small screens.-->
-            <div class="collapse navbar-collapse" id="menuFields">
-                <!--Fields on the left.-->
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Games<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="csgo.php">Counter-Strike: Global Offensive</a></li>
-                            <li><a href="dota2.php">Dota 2</a></li>
-                            <li><a href="starcraf2.php">Starcraft II</a></li>
-                        </ul>
-                    </li>
-                    
-                    <li><a href="#">About</a></li>
-
-                </ul>
-                <!--Fields on the right.-->
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Register</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-</br>
-
-
-<div id="content">
-=======
-  
->>>>>>> 972937d891ee31295351b47f549c33730932b0fa
+    <div id="wrapper">
+        <?php require_once 'template/header&navbar.php'; ?>
+        
+        <div id="content">
             <div class="container">
-               <center>
-                
-        <img src="about.png" class="img-circle" alt="Cinque Terre" width="300" height="220" style="margin-top:-20px">
-    </center>   
-     </br>
+                <ul id="about" class="list-group">
+                    <li class="list-group-item">
+									<center>            
+									<img src="about.png" class="img-circle" alt="Cinque Terre" width="300" height="220" style="margin-top:-20px">
+									</center>   
+								</br>
+								</div>
+								 <div class="row">
+								  <div class="col-md-4">
+									<div class="thumbnail"><img src="dh.jpg" alt="Counter Strike: Global Offensive thumbnail."/></div></a>
+										<h3 style="color: white; ">What is DreamhackChallenge?</h3>
+										<p style="color: white; ">Dreamhackchallenge is a website where you and your friends can check out the livestream and bet on games for free to win great prizes.
+										There are opportunities to watch various live stream on different games such as:
+										<li style="color: white;"> CS:GO</li>
+										<li style="color: white;"> DOTA2</li>
+										<li style="color: white;"> Starcraft II</li>
+										<p style="color: white; ">We have customized the website for all of you who like Dreamhack and e-sports event.
+										You have opportunities to:
+										<li style="color: white;"><b><i>Create profile</i></b></li>
+										<li style="color: white;"><b><i>Connect with facebook</i></b></li>
+										</p>
+									</p>
+								  </div>
+										<div class="col-md-4">
+											<div class="thumbnail"><img src="sport.jpg" alt="Esport"/></div></a>
 
-    </div>
+											<h3 style="color: white; ">How does it work?</h3>
+													<p style="color: white; "> With Dreamhackchallenge there are a few steps that has to de done, then you will be able to start betting and watch livestream.
+													<li style="color: white;"><b><i>Create account</i></b></li>
+													<li style="color: white;"><b><i>Create profile</i></b></li>
 
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="thumbnail"><img src="dh.jpg" alt="Counter Strike: Global Offensive thumbnail."/></div></a>
-                        <h3 style="color: white; ">What is DreamhackChallenge?</h3>
-<p style="color: white; ">Dreamhackchallenge is a website where you and your friends can check out the livestream and bet on games for free to win great prizes.
-There are opportunities to watch various live stream on different games such as:
-<li style="color: white;"> CS:GO</li>
-<li style="color: white;"> DOTA2</li>
-<li style="color: white;"> Starcraft II</li>
-<p style="color: white; ">We have customized the website for all of you who like Dreamhack and e-sports event.
-You have opportunities to:
-<li style="color: white;"><b><i>Create profile</i></b></li>
-<li style="color: white;"><b><i>Connect with facebook</i></b></li>
+													</br>
+													<p style="color: white; "> After this steps you will be able to choose which game you like to watch and bet on.
+													You simply press on the button "Games" and choose the type of game you like to watch, and then you have several of teams you can bet on. 
+													Beneath each team there is a bet button which you can press on and your betting will be applied. 
+													</p>
 
-</p>
-</p>
+																		</div>
+																		<div class="col-md-3">
+																		   <div class="thumbnail"><img src="dh2.jpg" alt="#"/></div></a>
 
-                    </div>
-                    <div class="col-md-4">
-                        <div class="thumbnail"><img src="sport.jpg" alt="Esport"/></div></a>
+													<h3 style="color: white; ">Company Info</h3>
+													<p style="color: white; "> We were founded 2015 in Sweden Stockholm, created by students at Stockholm Universitet.
+													Our main focus is about Dreamhack and E-sports events, we created a free betting and streaming site for you people to join to watch free livestream and free betting to win fine prizes.
 
-                        <h3 style="color: white; ">How does it work?</h3>
-<p style="color: white; "> With Dreamhackchallenge there are a few steps that has to de done, then you will be able to start betting and watch livestream.
-<li style="color: white;"><b><i>Create account</i></b></li>
-<li style="color: white;"><b><i>Create profile</i></b></li>
-
-</br>
-<p style="color: white; "> After this steps you will be able to choose which game you like to watch and bet on.
-You simply press on the button "Games" and choose the type of game you like to watch, and then you have several of teams you can bet on. 
-Beneath each team there is a bet button which you can press on and your betting will be applied. 
-</p>
-
-                    </div>
-                    <div class="col-md-3">
-                       <div class="thumbnail"><img src="dh2.jpg" alt="#"/></div></a>
-
-<h3 style="color: white; ">Company Info</h3>
-<p style="color: white; "> We were founded 2015 in Sweden Stockholm, created by students at Stockholm Universitet.
-Our main focus is about Dreamhack and E-sports events, we created a free betting and streaming site for you people to join to watch free livestream and free betting to win fine prizes.
-
-<li style="color: white;"><b><i>Created February 2015</i></b></li>
-<li style="color: white;"><b><i>Lanchued Juny 2015<i></b></li>
-</br>
-<p style="color: white; "> We are constantly developing the website to make it more accessible, more functions for the users.
-</p>
-                    </div>
-                </div>
+													<li style="color: white;"><b><i>Created February 2015</i></b></li>
+													<li style="color: white;"><b><i>Lanchued Juny 2015<i></b></li>
+													</br>
+											<p style="color: white; "> We are constantly developing the website to make it more accessible, more functions for the users. </p>
+										</div>
+									</div>
+								</div>
+                    </li>
+                </ul>
             </div>
         </div>
     
-        <?php require_once 'template/footer.php'; ?>
+    <?php require_once 'template/footer.php'; ?>
     </div>
 </body>
 </html>
