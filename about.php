b92850cd14e30464e161a07c77c20bdb47e973a6
Robert Norberg <robban_rulez@hotmail.com>
2015-05-25 15:53:05 +0200

    Mher

--- about.php
+++ about.php
@@ -21,16 +21,16 @@
                 <div class="row">
                     <div class="col-md-4">
                         <div class="thumbnail"><img src="images/dh.jpg" alt="Counter Strike: Global Offensive thumbnail."/>
-										<h3 style="color: white; ">What is Dreamhack Challenge?</h3>
-										<p style="color: white; ">Dreamhack Challenge is a website where you and your friends can check out the livestream and bet on games for free to win great prizes.
+										<h3 style="color: black; ">What is Dreamhack Challenge?</h3>
+										<p style="color: black; ">Dreamhack Challenge is a website where you and your friends can check out the livestream and bet on games for free to win great prizes.
 										There are opportunities to watch various live stream on different games such as:
-										<li style="color: white;"> CS:GO</li>
-										<li style="color: white;"> DOTA2</li>
-										<li style="color: white;"> Starcraft II</li>
-										<p style="color: white; ">We have customized the website for all of you who like Dreamhack and e-sports event.
+										<li style="color: black;"> CS:GO</li>
+										<li style="color: black;"> DOTA2</li>
+										<li style="color: black;"> Starcraft II</li>
+										<p style="color: black; ">We have customized the website for all of you who like Dreamhack and e-sports event.
 										You have opportunities to:
-										<li style="color: white;"><b><i>Create profile</i></b></li>
-										<li style="color: white;"><b><i>Connect with facebook</i></b></li>
+										<li style="color: black;"><b><i>Create profile</i></b></li>
+										<li style="color: black;"><b><i>Connect with facebook</i></b></li>
 										</p>
 						</div>
                     </div>
@@ -57,29 +57,29 @@
 								 <div class="row">
 								  <div class="col-md-4">
 									<div class="thumbnail"><img src="images/dh.jpg" alt="Counter Strike: Global Offensive thumbnail."/></div></a>
-										<h3 style="color: white; ">What is DreamhackChallenge?</h3>
-										<p style="color: white; ">Dreamhackchallenge is a website where you and your friends can check out the livestream and bet on games for free to win great prizes.
+										<h3 style="color: black; ">What is DreamhackChallenge?</h3>
+										<p style="color: black; ">Dreamhackchallenge is a website where you and your friends can check out the livestream and bet on games for free to win great prizes.
 										There are opportunities to watch various live stream on different games such as:
-										<li style="color: white;"> CS:GO</li>
-										<li style="color: white;"> DOTA2</li>
-										<li style="color: white;"> Starcraft II</li>
-										<p style="color: white; ">We have customized the website for all of you who like Dreamhack and e-sports event.
+										<li style="color: black;"> CS:GO</li>
+										<li style="color: black;"> DOTA2</li>
+										<li style="color: black;"> Starcraft II</li>
+										<p style="color: black; ">We have customized the website for all of you who like Dreamhack and e-sports event.
 										You have opportunities to:
-										<li style="color: white;"><b><i>Create profile</i></b></li>
-										<li style="color: white;"><b><i>Connect with facebook</i></b></li>
+										<li style="color: black;"><b><i>Create profile</i></b></li>
+										<li style="color: black;"><b><i>Connect with facebook</i></b></li>
 										</p>
 									</p>
 								  </div>
 										<div class="col-md-4">
 											<div class="thumbnail"><img src="images/sport.jpg" alt="Esport"/></div></a>
 
-											<h3 style="color: white; ">How does it work?</h3>
-													<p style="color: white; "> With Dreamhackchallenge there are a few steps that has to de done, then you will be able to start betting and watch livestream.
-													<li style="color: white;"><b><i>Create account</i></b></li>
-													<li style="color: white;"><b><i>Create profile</i></b></li>
+											<h3 style="color: black; ">How does it work?</h3>
+													<p style="color: black; "> With Dreamhackchallenge there are a few steps that has to de done, then you will be able to start betting and watch livestream.
+													<li style="color: black;"><b><i>Create account</i></b></li>
+													<li style="color: black;"><b><i>Create profile</i></b></li>
 
 													</br>
-													<p style="color: white; "> After this steps you will be able to choose which game you like to watch and bet on.
+													<p style="color: black; "> After this steps you will be able to choose which game you like to watch and bet on.
 													You simply press on the button "Games" and choose the type of game you like to watch, and then you have several of teams you can bet on. 
 													Beneath each team there is a bet button which you can press on and your betting will be applied. 
 													</p>
@@ -88,14 +88,14 @@
 																		<div class="col-md-3">
 																		   <div class="thumbnail"><img src="images/dh2.jpg" alt="#"/></div></a>
 
-													<h3 style="color: white; ">Company Info</h3>
-													<p style="color: white; "> We were founded 2015 in Sweden Stockholm, created by students at Stockholm Universitet.
+													<h3 style="color: black; ">Company Info</h3>
+													<p style="color: black; "> We were founded 2015 in Sweden Stockholm, created by students at Stockholm Universitet.
 													Our main focus is about Dreamhack and E-sports events, we created a free betting and streaming site for you people to join to watch free livestream and free betting to win fine prizes.
 
-													<li style="color: white;"><b><i>Created February 2015</i></b></li>
-													<li style="color: white;"><b><i>Lanchued Juny 2015<i></b></li>
+													<li style="color: black;"><b><i>Created February 2015</i></b></li>
+													<li style="color: black;"><b><i>Lanchued Juny 2015<i></b></li>
 													</br>
-											<p style="color: white; "> We are constantly developing the website to make it more accessible, more functions for the users. </p>
+											<p style="color: black; "> We are constantly developing the website to make it more accessible, more functions for the users. </p>
 										</div>
 									</div>
 								</div>
