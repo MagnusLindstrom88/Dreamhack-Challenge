3dd8fcc50b205d78dc10e5d29397f203bc3ab5a7
Firstname Surname <sebwar01@hotmail.com>
2015-05-26 17:07:53 +0200

    Liten fix

--- scripts/login&registration/verify_account.php
+++ scripts/login&registration/verify_account.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'scripts/init.php';
+require_once '../init.php';
 $ps = $db->prepare("UPDATE users SET verification_key = null WHERE verification_key = ?");
 $ps->execute(array($_GET['key']));
 
