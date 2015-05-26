dcd5625f3f77fd6435ea702e2feb4983c159317b
Sebwar <sebwar01@hotmail.com>
2015-05-26 21:31:40 +0200

    Update chat.php

--- scripts/chat.php
+++ scripts/chat.php
@@ -1,10 +1,5 @@
 <?php
-session_start();
 
-
-    $message = $_POST['message'];
- 
         $chatlog = fopen("../chatlog.html", "a");
-        fwrite($chatlog, "<div>{$_SESSION['username']}: {$message}</div>");
+        fwrite($chatlog, "<div>asddsa</div>");
         fclose($chatlog);
- 
