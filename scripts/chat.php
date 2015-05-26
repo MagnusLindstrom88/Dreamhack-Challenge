a616e9fe60bc066c7f293a4508d710d17831def9
Sebwar <sebwar01@hotmail.com>
2015-05-26 21:33:34 +0200

    Update chat.php

--- scripts/chat.php
+++ scripts/chat.php
@@ -1,5 +1,12 @@
 <?php
+session_start();
 
-        $chatlog = fopen("chatlog.html", "a");
-        fwrite($chatlog, "<div>asddsa</div>");
+if(isset($_SESSION['username'])) {
+    $message = trim($_POST['message']);
+    if(strlen($message) > 0) {
+        $chatlog = fopen("../chatlog.html", "a");
+        fwrite($chatlog, "<div>{$_SESSION['username']}: {$message}</div>");
         fclose($chatlog);
+    }
+}
+else echo "failed";
