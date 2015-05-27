c46c47d63a710dd864bd18e03847845c4e5d7c16
Sebwar <sebwar01@hotmail.com>
2015-05-27 22:59:03 +0200

    Update chat.php

--- scripts/chat.php
+++ scripts/chat.php
@@ -4,6 +4,6 @@ session_start();
 if(isset($_SESSION['username'])) {
     $message = trim($_POST['message']);
     if(strlen($message) > 0)
-        header("location: http://www.avetian.se/skola/chat.php?username={$_SESSION['username']}&message={$message}") or die("I am a shit server.");
+        header("location: http://www.avetian.se/skola/chat.php?username={$_SESSION['username']}&message={$message}");
 }
 else echo "failed";
