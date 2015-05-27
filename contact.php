846d2bba9a2c3053efa1183b175541929c84c046
bryo1337 <simon_Palmqvist@hotmail.com>
2015-05-27 13:32:16 +0200

    contact form

--- contact.php
+++ contact.php
@@ -89,7 +89,7 @@
                         <form>
                           <div class="form-group">
                             <label for="name" class="control-label">Your name:</label>
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Your name" value="<?php if($name) {echo $name} else { echo htmlspecialchars($_POST['name']);} ?>">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Your name" value="<?php if(!empty($name){ echo $name}?>">
                             <?php echo "<p class='text-danger'>$nameError</p>";?>
                           </div>
                           <div class="form-group">
