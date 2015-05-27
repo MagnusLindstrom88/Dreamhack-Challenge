d860ef8628473c2f1645d7eeb212e250a4915d56
Sebwar <sebwar01@hotmail.com>
2015-05-27 17:34:02 +0200

    Update stream.php

--- stream.php
+++ stream.php
@@ -51,7 +51,7 @@
 		<div class="row">
 		    <div class="col-md-8">
 		    	<div id="video-wrapper">
-		    	    <iframe src="https://www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf?channel=esl_csgo" frameborder="0"></iframe>
+		    	    <iframe src="https://www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf?channel=fragbitelive" frameborder="0"></iframe>
 		    	</div>
 		    </div>
 		    <div class="col-md-4"  id="chat-container">
@@ -76,7 +76,7 @@
     <script>
 	//Switch to a non-https embed for iOS devices since they apparently won't display it otherwise.
     	if (/iPhone|iPad|iPod/i.test(navigator.userAgent) )
-    		$("#video-wrapper")[0].innerHTML = " <iframe src='http://www.twitch.tv/imaqtpie/embed' frameborder='0' scrolling='no'></iframe>";
+    		$("#video-wrapper")[0].innerHTML = " <iframe src='http://www.twitch.tv/fragbitelive/embed' frameborder='0' scrolling='no'></iframe>";
 	
 	//Refreshes the chat log periodically.
 	setInterval(loadChatLog, 2000);
