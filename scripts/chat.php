bd41852fbced9bd9f3a2ca5054208ee3d1f792b2
Firstname Surname <sebwar01@hotmail.com>
2015-05-27 23:18:40 +0200

    testar fix för chatt

--- scripts/chat.php
+++ scripts/chat.php
@@ -3,7 +3,10 @@ session_start();
 
 if(isset($_SESSION['username'])) {
     $message = trim($_POST['message']);
-    if(strlen($message) > 0)
-        header("location: http://www.avetian.se/skola/chat.php?username={$_SESSION['username']}&message={$message}");
+    if(strlen($message) > 0) {
+        $fh = fopen("http://www.avetian.se/skola/chat.php?username={$_SESSION['username']}&message={$message}", 'r');
+        fclose($fh);
+    }
+        //header("location: http://www.avetian.se/skola/chat.php?username={$_SESSION['username']}&message={$message}");
 }
 else echo "failed";
