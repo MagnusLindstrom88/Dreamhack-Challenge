0e850ea857867f0ba91f5a51f2b36f98259ec583
Sebwar <sebwar01@hotmail.com>
2015-05-27 17:29:54 +0200

    Update stream.php

--- stream.php
+++ stream.php
@@ -51,16 +51,15 @@
 		<div class="row">
 		    <div class="col-md-8">
 		    	<div id="video-wrapper">
-		    		<iframe src="https://www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf?channel=esl_csgo" frameborder="0"></iframe>
-		    	   
+		    	    <iframe src="https://www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf?channel=esl_csgo" frameborder="0"></iframe>
 		    	</div>
 		    </div>
 		    <div class="col-md-4"  id="chat-container">
-				<div id="chatbox">
-					<div id="chat-message-area"></div>
-					<textarea id="chat-input-field" rows="1"></textarea>
-					<button class="btn btn-default pull-right" onclick="sendMessage()">Send</button>
-				</div>
+			<div id="chatbox">
+			    <div id="chat-message-area"></div>
+			    <textarea id="chat-input-field" rows="1"></textarea>
+			    <button class="btn btn-default pull-right" onclick="sendMessage()">Send</button>
+			</div>
 		    </div>
 		</div>
 		<div class="row">
@@ -77,7 +76,7 @@
     <script>
 	//Switch to a non-https embed for iOS devices since they apparently won't display it otherwise.
     	if (/iPhone|iPad|iPod/i.test(navigator.userAgent) )
-    		$("#video-wrapper")[0].innerHTML = " <iframe src='http://www.twitch.tv/imaqtpie/embed' frameborder='0' scrolling='no'></iframe>";
+    		$("#video-wrapper")[0].innerHTML = " <iframe src='http://www.twitch.tv/esl_csgo/embed' frameborder='0' scrolling='no'></iframe>";
 	
 	//Refreshes the chat log periodically.
 	setInterval(loadChatLog, 2000);
