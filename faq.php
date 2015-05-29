a2f38de9499a15bdaafa380a9246928bc4ddc6ce
Fahlle <Fahlle@users.noreply.github.com>
2015-05-29 12:26:42 +0200

    Update faq.php
    FAQ-texten fixad

--- faq.php
+++ faq.php
@@ -20,54 +20,38 @@
                 
                 <ul id="faq-list" class="list-group">
                     <li class="list-group-item">
-                        <h4 class="list-group-item-heading">Consectetur adipiscing elit?</h4>
-                        <p class="list-group-item-text">Quisque semper, arcu vel convallis mollis, urna sapien varius lectus, vel dignissim purus odio ac libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin elementum volutpat elit. Ut a rhoncus quam.</p>
+                        <h4 class="list-group-item-heading">What is Dreamhack Challenge?</h4>
+                        <p class="list-group-item-text">Dreamhack Challenge is a meeting place for anyone who is interested in gaming an e-sports where you can follow the event and interact, compete and bet with others.</p>
                     </li>
                     <li class="list-group-item">
-                        <h4 class="list-group-item-heading">Etiam sed nisl eu tellus vulputate, pretium quis non sapien?</h4>
-                        <p class="list-group-item-text">Phasellus lacinia dui vel magna tristique dapibus. Donec pretium turpis ultrices scelerisque volutpat. Integer quis commodo velit. Sed ornare augue vel ligula dignissim, ac venenatis turpis ullamcorper. Nam laoreet sagittis lacus eu sodales. Donec quam quam, congue non ultrices in, sagittis sit amet urna. Aliquam erat volutpat. Donec laoreet elit a sapien scelerisque interdum. Curabitur lorem libero, maximus eleifend tellus non, pharetra laoreet lorem.</p>
+                        <h4 class="list-group-item-heading">How do I login?</h4>
+                        <p class="list-group-item-text">In order to login you have to be registered, you can register either by creating an account on Dreamhack Challenge or by registering with your Facebook account.</p>
                     </li>
                     <li class="list-group-item">
-                        <h4 class="list-group-item-heading">In orci turpis?</h4>
-                        <p class="list-group-item-text">Consequat vestibulum orci vitae, vestibulum tempus odio?</p>
+                        <h4 class="list-group-item-heading">How does the betting work?</h4>
+                        <p class="list-group-item-text">When signed in you simply click on the game that your interested in, on the page of each respective supported game the matches to be played will appear. Simply click on the bet-button on the team you believe is going to win to place your bet. .</p>
                     </li>
                     <li class="list-group-item">
-                        <h4 class="list-group-item-heading">Donec tempus mollis justo id suscipit?</h4>
-                        <p class="list-group-item-text">Maecenas a sapien vehicula, ultricies nunc quis, scelerisque dui. Cras ut vulputate nunc. Duis ac lectus quis nibh finibus pretium venenatis vitae diam. Vivamus venenatis congue urna, ac pharetra sem aliquam non. In hac habitasse platea dictumst. Vestibulum eget dolor vel justo ornare scelerisque. Morbi ultrices efficitur felis, non finibus leo tincidunt volutpat. Praesent auctor interdum lectus nec tincidunt. Duis tincidunt varius bibendum. Vestibulum lacinia ipsum risus, nec fermentum ante convallis non.</p>
+                        <h4 class="list-group-item-heading">What are your supported games?</h4>
+                        <p class="list-group-item-text">The three biggest ones on the Dreamhack event, that is Dota 2, Counterstrike:Global offensive and Starcraft 2.</p>
+                    </li>
+                    
+                    <li class="list-group-item">
+                        <h4 class="list-group-item-heading">I am under 18, can I still bet?</h4>
+                        <p class="list-group-item-text">Yes! Since there is no money involved in the betting you can be under 18.</p>
                     </li>
                     <li class="list-group-item">
-                        <h4 class="list-group-item-heading">Nullam pretium aliquet scelerisque?</h4>
-                        <p class="list-group-item-text">Proin ut erat risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In hac habitasse platea dictumst.</p>
+                        <h4 class="list-group-item-heading">Do you stream the matches?</h4>
+                        <p class="list-group-item-text">Yes we do. Open up the page of a supported game and click on the stream-button on the navbar to open up the stream.</p>
                     </li>
                     <li class="list-group-item">
-                        <h4 class="list-group-item-heading">Pellentesque id euismod dui, id vulputate ligula?</h4>
-                        <p class="list-group-item-text">Curabitur pretium semper congue. Praesent in consectetur est. Sed leo ex, laoreet quis pellentesque a, iaculis sit amet leo. Nunc ornare ut augue ut tristique. Etiam iaculis laoreet elit facilisis tincidunt. Duis eu nisi tempus, cursus erat eget, rutrum libero.</p>
+                        <h4 class="list-group-item-heading">How do I change my user information or profile picture?</h4>
+                        <p class="list-group-item-text">When signed in, you simply click on the my pages-button on the navbar to edit your personal information.</p>
                     </li>
                     <li class="list-group-item">
-                        <h4 class="list-group-item-heading">Pellentesque velit magna?</h4>
-                        <p class="list-group-item-text">Convallis eu vehicula eget, hendrerit eget mauris. Donec sit amet elit ut metus pellentesque euismod. Pellentesque ut condimentum ipsum, hendrerit aliquet tellus.</p>
-                    </li>
-                    <li class="list-group-item">
-                        <h4 class="list-group-item-heading">Fusce vitae magna posuere?</h4>
-                        <p class="list-group-item-text">Accumsan nisl id, congue eros. Phasellus felis ipsum, ornare sed nisl non, volutpat volutpat ante. Nam accumsan est nibh, quis commodo purus condimentum in. Mauris vel nulla sagittis eros ullamcorper imperdiet sit amet vel ligula.</p>
-                    </li>
-                    <li class="list-group-item">
-                        <h4 class="list-group-item-heading">Duis condimentum ligula?</h4>
-                        <p class="list-group-item-text">Etiam placerat fringilla ullamcorper. Mauris tincidunt ligula finibus, ullamcorper lorem a, tincidunt nulla. Vestibulum nulla odio, vehicula et quam nec, posuere blandit lectus.</p>
-                    </li>
-                    <li class="list-group-item">
-                        <h4 class="list-group-item-heading">Aenean pulvinar lorem in felis convallis?</h4>
-                        <p class="list-group-item-text">Sed ac lorem vitae ipsum pretium ornare. Ut a blandit elit. Nullam sapien purus, dictum id ullamcorper eu, vulputate a lorem. Praesent rutrum iaculis lectus, et lobortis magna vulputate in. Sed sed feugiat metus. Nam mattis id mauris ut pulvinar. Pellentesque bibendum dui ligula, consectetur pulvinar mauris pellentesque nec. Quisque eu molestie ipsum. Nulla non mattis purus.</p>
-                    </li>
-                    <li class="list-group-item">
-                        <h4 class="list-group-item-heading">Donec interdum tellus at bibendum?</h4>
-                        <p class="list-group-item-text">Mauris dapibus, ex eu consectetur feugiat, turpis magna viverra nibh, non vestibulum nunc libero imperdiet mauris. Sed consectetur erat nec placerat pharetra. Sed iaculis luctus leo, sit amet condimentum elit interdum ut. Ut molestie nec nibh eget pretium. Cras euismod quam et nisl dignissim, et egestas lacus ullamcorper.</p>
+                        <h4 class="list-group-item-heading">Is Dreamhack Challenge created by Dreamhack officially?</h4>
+                        <p class="list-group-item-text">No, Dreamhack Challange is an independent meeting place created by Students from Stockholm University.</p>
                     </li>
                 </ul>
             </div>
         </div>
-    
-    <?php require_once 'template/footer.php'; ?>
-    </div>
-</body>
-</html>
