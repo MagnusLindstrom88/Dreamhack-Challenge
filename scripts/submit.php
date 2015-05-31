c98496606aa82568a4aaad0fd56d8d36676e603c
bryo1337 <simon_Palmqvist@hotmail.com>
2015-05-31 16:43:10 +0200

    contact form

--- scripts/submit.php
+++ scripts/submit.php
@@ -1,37 +1,37 @@
 <?php
 require_once '../init.php';
+if(isset($_POST['email'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $captcha = $_POST['g-recaptcha-response'];
+   
+    $from = 'Dreamhack challenge contact form';
+    $to = 'simon_palmqvist@hotmail.com';
+    $subject = 'Message from ' . $name;
+    $body ="From: $name\n E-Mail: $email\n Message:\n $message";
+    
+    //Verify the captcha by sending a POST-request to Google's server.
+    $url = 'https://www.google.com/recaptcha/api/siteverify';
+    $data = array('secret' => '6LfzwQYTAAAAAAkbkImDqmDj6l27DDwdXIGqtVv5', 'response' => $captcha);
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data),
+        ),
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    $result = json_decode($result, true);
+    
+    //If the verification of the captcha was successful.
+    if($result['success']) {
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$captcha = $_POST['g-recaptcha-response'];
+        mail($to, $subject, $body, $from)
 
-$from = 'Dreamhack challenge contact form';
-$to = 'simon_palmqvist@hotmail.com';
-$subject = 'Message from ' . $name;
-$body ="From: $name\n E-Mail: $email\n Message:\n $message";
-
-//Verify the captcha by sending a POST-request to Google's server.
-$url = 'https://www.google.com/recaptcha/api/siteverify';
-$data = array('secret' => '6LfzwQYTAAAAAAkbkImDqmDj6l27DDwdXIGqtVv5', 'response' => $captcha);
-$options = array(
-    'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
-        'content' => http_build_query($data),
-    ),
-);
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-$result = json_decode($result, true);
-
-//If the verification of the captcha was successful.
-if($result['success']) {
-
-    mail($to, $subject, $body, $from)
-
-    echo "Thank you! Your message has been received.";
+        echo "Thank you! Your message has been received.";
+    }
+    else echo "Failed to submit, please try again.";
 }
-else echo "Failed to submit, please try again.";
-
 
