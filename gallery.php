b91c5fd9eea35d29ebd898e352fc185e343160e4
Alkesten <o.Alkesten@Gmail.com>
2015-06-01 17:53:50 +0200

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -10,7 +10,15 @@
         </br>
             <div class='row'>
             <?php
+            $count=0;
                 foreach(glob("images/*.*") as $image){
+                    $count+=1;
+                    if($count==4){
+                        $count = 0;
+                        echo "
+                                <div class='row'>
+                             ";
+                    }
                     echo "
                             <div class='col-md-3 col-sm-6'>
                                 <img src='$image' class='thumbnail img-responsive' style='border:1px solid red; width:250px; height:250px;'>
