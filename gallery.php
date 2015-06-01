b9238a88152ea0f5fbacb0f602f3c45525b7b07e
Alkesten <o.Alkesten@Gmail.com>
2015-06-01 22:34:06 +0200

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -13,13 +13,19 @@
             $count=0;
                 foreach(glob("images/*.*") as $image){
                     $count+=1;
-                    if($count<4){
+                    if($count==4){
+                        $count=0;
                         echo "
                                 <div class='row'>
                                     <div class='col-md-3 col-sm-6'>
                                         <img src='$image' class='thumbnail img-responsive' style='border:1px solid red; width:250px; height:250px;'>
                                     </div> 
-                                </div>
+                             ";
+                    }else{
+                        echo "
+                                <div class='col-md-3 col-sm-6'>
+                                    <img src='$image' class='thumbnail img-responsive' style='border:1px solid red; width:250px; height:250px;'>
+                                </div> 
                              ";
                     }
                 }?>
