b8b4e787e16b1f65cb8e38d5d792c78fbb17204a
sebwar <sebwar01@hotmail.com>
2015-06-01 17:07:03 +0200

    Borttagning av onödig kod i bettingfunktionen. Omdöpning av vissa variabler.

--- scripts/bet_functions.js
+++ scripts/bet_functions.js
@@ -2,7 +2,7 @@
 function makeBet(btn){
     var jax = new XMLHttpRequest();
     var team = btn.getAttribute("id");
-    var matchen = btn.parentNode.getAttribute("id");
+    var match = btn.parentNode.getAttribute("id");
     
     jax.open("POST", "scripts/make_bet.php", true)
     jax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -10,36 +10,12 @@ function makeBet(btn){
     jax.onload = function() {
         var return_data = jax.responseText;
         
-        if (return_data == "a")
+        if (return_data === "a")
           $(btn).removeClass().addClass("btn btn-info");
-        if (return_data == "c")
+        if (return_data === "c")
             $(btn).removeClass().addClass("btn btn-success");
-        if (return_data == "x")
+        if (return_data === "x")
             alert("You must be logged in to make a bet.");
     }
-    jax.send("team="+team+"&matchen="+matchen);
+    jax.send("team="+team+"&match="+match);
 }
-
-//Kollar vilka bets som har gjorts och ändrar färgen på de knappar som har bets registrerade.
-function showBet() {
-    var buttons = document.getElementsByClassName("btn btn-info");
-    for(var i = 0; i < buttons.length; i++)
-        get(buttons[i]);
-}
-
-function get(button){
-    var jax = new XMLHttpRequest();
-    var url = "scripts/check_bet.php";
-    var team = button.id;
-    var matchen = button.parentNode.getAttribute("id");
-    var data = "team="+team+"&matchen="+matchen;
-    
-    jax.open("POST",url,true)
-    jax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    jax.onload = function() {
-        if (jax.responseText == "a")
-        $(button).removeClass().addClass("btn btn-success");
-    }
-    jax.send(data);
-}
-     
--- scripts/make_bet.php
+++ scripts/make_bet.php
@@ -1,30 +1,30 @@
 <?php
 require_once 'init.php';
 if(isset($_SESSION['id'])) {
-	$member = $_SESSION['id'];
-	$matchen = $_POST["matchen"];
+	$user = $_SESSION['id'];
+	$match = $_POST["match"];
 	$team = $_POST["team"];
 	
 	$ps = $db->prepare("SELECT * FROM bets WHERE user_id=? AND match_id=?");
-	$ps->execute(array($member, $matchen));
+	$ps->execute(array($user, $match));
 	
 	if($ps->rowCount() != 0) {
 		$ps2 = $db->prepare("SELECT * FROM bets WHERE user_id=? AND match_id=? AND team_id=?");
-		$ps2->execute(array($member, $matchen, $team));
+		$ps2->execute(array($user, $match, $team));
 		if($ps2->rowCount() != 0) {
 			$sql = "DELETE FROM bets WHERE team_id=? AND user_id=? AND match_id=? ";
 			$query = $db->prepare($sql);
-			$query->execute(array($team,$member,$matchen));
+			$query->execute(array($team,$user,$match));
 			echo ("a");
 		}
 	}
 	else {
-		$sql = "INSERT INTO bets (team_id, user_id, match_id) Values (:team, :member, :matchen)";
+		$sql = "INSERT INTO bets (team_id, user_id, match_id) Values (:team, :member, :match)";
 		$query = $db->prepare($sql);
 		$query->execute(array(
 			":team" =>$team,
-			":member"=>$member,
-			":matchen"=>$matchen
+			":member"=>$user,
+			":match"=>$match
 		));
 		echo ("c");
 	}
