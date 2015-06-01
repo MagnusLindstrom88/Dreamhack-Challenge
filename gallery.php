b63c7a8a4e46b055d1e5b9135de261288f1672b1
Alkesten <o.Alkesten@Gmail.com>
2015-06-01 17:06:42 +0200

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -16,7 +16,7 @@
                 foreach($images as $image){
                     echo "
                             <div class='col-lg-4 col-sm-6 col-xs-12'>
-                                <img src='$image' class='thumbnail img-responsive' style='width:inherit;'>
+                                <img src='$image' class='thumbnail' style='width:inherit;'>
                             </div
                         ";
                 }?>
