3704c5dfd2d65c0ec88434204d8dc1d87d5d389e
Alkesten <o.Alkesten@Gmail.com>
2015-06-01 16:25:50 +0200

    Create gallery.php

---
+++ gallery.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once 'template/contact_head.php'; ?>
+</head>
+<body>
+    <div id="wrapper">
+        <?php require_once 'template/contact_header&navbar.php'; ?>
+        <div class="container">
+            <div class="row">
+              <div class="col-lg-4 col-sm-6 col-xs-12">
+                  <a href="#">
+                       <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+                  </a>
+              </div>
+               <div class="col-lg-4 col-sm-6 col-xs-12">
+                  <a href="#">
+                       <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+                  </a>
+              </div>
+               <div class="col-lg-4 col-sm-6 col-xs-12">
+                  <a href="#">
+                       <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+                  </a>
+              </div>
+               <div class="col-lg-4 col-sm-6 col-xs-12">
+                  <a href="#">
+                       <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+                  </a>
+              </div>
+               <div class="col-lg-4 col-sm-6 col-xs-12">
+                  <a href="#">
+                       <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+                  </a>
+              </div>
+               <div class="col-lg-4 col-sm-6 col-xs-12">
+                  <a href="#">
+                       <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+                  </a>
+              </div>
+               <div class="col-lg-4 col-sm-6 col-xs-12">
+                  <a href="#">
+                       <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+                  </a>
+              </div>
+               <div class="col-lg-4 col-sm-6 col-xs-12">
+                  <a href="#">
+                       <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+                  </a>
+              </div>
+               <div class="col-lg-4 col-sm-6 col-xs-12">
+                  <a href="#">
+                       <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+                  </a>
+        </div>
+      </div>
+    </div>
+        <?php require_once 'template/footer.php'; ?>
+    </div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+  <div class="row">
+    <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+        </a>
+    </div>
+     <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+        </a>
+    </div>
+     <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+        </a>
+    </div>
+     <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+        </a>
+    </div>
+     <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+        </a>
+    </div>
+     <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+        </a>
+    </div>
+     <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+        </a>
+    </div>
+     <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+        </a>
+    </div>
+     <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
+        </a>
+    </div>
+  </div>
+  <hr>
+  	<h2 class="text-center"><a href="http://www.bootply.com/d08bOti6Zs">Edit on Bootply</a></h2>
+  <hr>
+</div>
