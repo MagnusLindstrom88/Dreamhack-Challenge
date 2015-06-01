5dff8f693944930f7f3c6ebc8ecb3d589666eef2
Alkesten <o.Alkesten@Gmail.com>
2015-06-01 17:21:00 +0200

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -7,20 +7,20 @@
     <div id="wrapper">
         <?php require_once 'template/contact_header&navbar.php'; ?>
         <div class="container">
-        <div class="row">
         </br>
-            <div class='col-lg-3 col-md-4 col-xs-6 thumb'>
             <?php
                 $directory = "images/";
                 $images = glob($directory . "*.*");
                 
                 foreach($images as $image){
                     echo "
-                            <img src='$image' class='img-responsive' height='40' widht='40'>
+                            <div class="row">
+                                <div class='col-lg-3 col-md-4 col-xs-6 thumb'>
+                                    <img src='$image' class='img-responsive' height='40' widht='40'>
+                                </div>    
+                            </div>
                         ";
                 }?>
-             </div>    
-        </div>
         </div>
     </div>
     <?php require_once 'template/footer.php'; ?>
