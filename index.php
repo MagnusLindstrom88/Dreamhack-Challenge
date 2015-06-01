df0c489f11c10be9a3277416a9c7015ec9a57fa8
sebwar <sebwar01@hotmail.com>
2015-06-01 16:01:08 +0200

    index återställd

--- index.php
+++ index.php
@@ -3,14 +3,6 @@
 <head>
     <?php require_once 'template/head.php'; ?>
     <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/shift.css" rel="stylesheet">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
-    $("button").click(function(){
-        $("p").toggle();
-    });
-});
-</script>
     <style>
         .jumbotron {
             background-image: url(images/jumbotron.jpg);
@@ -36,20 +28,15 @@ $(document).ready(function(){
             <div class="container">
                  <h1>Welcome!</h1>
                  <p>Welcome to DreamHack Challenge! This is an interactive meeting place for gamers and e-sport enthusiasts. Follow the Dreamhack event from here, watch livestreams and place bets. Enjoy!</p>
-                 
-<button>Toggle</button>
-
-<p>This is a paragraph with little content.</p>
-<p>This is another small paragraph.</p>
-
-</div>
-           
+                 <a href="#">Learn More</a>
+            </div>
+        </div>
         
         <!--Content columns.-->
         <div id="content">
             <div class="container">
                 <h2>Our Supported Games</h2>
-                <p>Welcome to our supported games, feel free to place a bet and enjoy!</p>
+                <p>Fusce consequat mauris sed nisi dignissim, eu sagittis odio pellentesque.</p>
                 <div class="row">
                     <div class="col-md-4">
                         <a href="csgo.php"><div class="thumbnail"><img src="images/csgothumbnail.jpg" alt="Counter Strike: Global Offensive thumbnail."/></div></a>
