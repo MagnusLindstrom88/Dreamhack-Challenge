0f4aa0784dd9e7d4be1732ecb8958404ff8cf666
Robert <robban_rulz@hotmail.com>
2015-06-02 16:02:17 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,17 +13,37 @@
     <?php require_once 'template/header&navbar.php'; ?>
     <div id="wrapper">
 	<div class="container">
-	<?php
-	global $db;
-	$matches = $db->query(	"SELECT *
+			<?php
+			$servername = "mysql.dsv.su.se";
+			$username = "sewa2700";
+			$password = "eequishusaiz";
+			$dbname = "sewa2700";
+			
+			// Create connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
+			// Check connection
+			if ($conn->connect_error) {
+			    die("Connection failed: " . $conn->connect_error);
+			}
+			
+			$sql = "SELECT *
 				FROM matches, bets, users
 				WHERE matches.id = bets.match_id
 				AND bets.user_id = users.id
-				");
-    	foreach($matches as $row) {
-     		echo $row[users.id];
-	}
-	?>
+				";
+				
+			$result = $conn->query($sql);
+			
+			if ($result->num_rows > 0) {
+			    while($row = $result->fetch_assoc()) {
+			        echo "id: " . $row["users.id"]. " - Name: " . $row["matches.id"]."<br>";
+			    }
+			} else {
+			    echo "0 results";
+			}
+			$conn->close();
+			?> 
+
         </div>      	
   </div>
   <?php require_once 'template/footer.php'; ?>
