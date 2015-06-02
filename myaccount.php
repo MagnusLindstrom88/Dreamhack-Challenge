73679c67cf8e6304f4c2f51971a722df0aa3a53d
Alkesten <o.Alkesten@Gmail.com>
2015-06-02 16:00:00 +0200

    Update myaccount.php

--- myaccount.php
+++ myaccount.php
@@ -162,7 +162,7 @@ function generateBoxesAccount($game) {
     	$teams = $db->query("SELECT * FROM teams WHERE (teams.id={$row['team0']} OR teams.id={$row['team1']}")->fetchALL(PDO::FETCH_ASSOC);
         $buttonClass0 = "btn btn-info";
         $buttonClass1 = "btn btn-info";
-        $matchBoxClass = "match-box-account";
+        $matchBoxClass = "match-box upcoming";
 
         if(isset($_SESSION['id'])) {
             $bet = $db->query("SELECT * FROM bets WHERE user_id={$_SESSION['id']} AND match_id={$row['id']} AND (team_id={$teams[0]['id']} OR team_id={$teams[1]['id']})");
@@ -199,13 +199,13 @@ function generateBoxesAccount($game) {
         "
         <div class='col-md-3 col-sm-6'>
             <div class='{$matchBoxClass}' id='{$row['id']}'>
-                <div class='match-header-account'>
+                <div class='match-header'>
                     <p>{$teams[0]['name']} VS {$teams[1]['name']}</p>
                 </div>
-                <div class='match-logos-account'>
-                    <img class='team-logo-account' src='images/teamlogos/{$teams[0]['abbreviation']}.png' alt=\"{$teams[0]['name']}'s logotype.\"/>
-                    <img class='versus-account' src='images/vs.png' alt='Versus.'/>
-                    <img class='team-logo-account' src='images/teamlogos/{$teams[1]['abbreviation']}.png' alt=\"{$teams[1]['name']}'s logotype.\"/>
+                <div class='match-logos'>
+                    <img class='team-logo' src='images/teamlogos/{$teams[0]['abbreviation']}.png' alt=\"{$teams[0]['name']}'s logotype.\"/>
+                    <img class='versus' src='images/vs.png' alt='Versus.'/>
+                    <img class='team-logo' src='images/teamlogos/{$teams[1]['abbreviation']}.png' alt=\"{$teams[1]['name']}'s logotype.\"/>
                 </div>
                 <button class='{$buttonClass0}' id='{$teams[0]['id']}' onclick='makeBet(this)' style='margin-right: 5px;'>Bet {$teams[0]['abbreviation']}</button>
                 <button class='{$buttonClass1}' id='{$teams[1]['id']}' onclick='makeBet(this)' style='margin-left: 5px;'>Bet {$teams[1]['abbreviation']}</button>
