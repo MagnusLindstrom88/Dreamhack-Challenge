d0ec68d2f8d9bd91d959decfbfd2cd73a200720d
bryo1337 <simon_Palmqvist@hotmail.com>
2015-06-02 15:07:38 +0200

    La till gallery

--- template/header&navbar.php
+++ template/header&navbar.php
@@ -35,6 +35,7 @@
                 <li><a href="faq.php">FAQ</a></li>
                 <li><a href="about.php">About Us</a></li>
                 <li><a href="contact.php">Contact Us</a></li>
+                <li><a href="gallery.php">Gallery</a></li>
             </ul>
             <?php
             //Fields on the right. Changes depending on if the user is logged in.
