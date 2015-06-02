38ae31889e8c4298fb3c313b60c70557bf182eb0
Robert <robban_rulz@hotmail.com>
2015-06-02 15:55:45 +0200

    Update test.php

--- test.php
+++ test.php
@@ -11,25 +11,20 @@
     <?php require_once 'template/header&navbar.php'; ?>
     <div id="wrapper">
 	<div class="container">
-			<?php generateBoxesAccount(); ?>
+	global $db;
+	$matches = $db->query(	"SELECT *
+				FROM matches, bets, users
+				WHERE matches.id = bets.match_id
+				AND bets.user_id = users.id
+				AND matches.game = 'CS:GO'
+				AND users.id = '10206612626732341'
+				");
+    	foreach($matches as $row) {
+     		echo $row[users.id];
+	}
         </div>      	
   </div>
   <?php require_once 'template/footer.php'; ?>
 </body>
 </html>
 
-<?php
-function generateBoxesAccount() {
-    global $db;
-    $matches = $db->query(	"SELECT *
-				FROM matches, bets, users
-				WHERE matches.id = bets.match_id
-				AND bets.user_id = users.id
-				AND matches.game = "CS:GO"
-				AND users.id = "10206612626732341"
-				LIMIT 0 , 30");
-    foreach($matches as $row) {
-     echo $row[users.id];
-    }
-}
-?>
