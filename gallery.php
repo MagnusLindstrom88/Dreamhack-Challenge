c88a8e12cfa10df078871955b248a01188fb54f6
Firstname Surname <sebwar01@hotmail.com>
2015-06-02 13:52:09 +0200

    Row-problemet fixat.

--- gallery.php
+++ gallery.php
@@ -1,33 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php require_once 'template/contact_head.php'; ?>
+    <?php require_once 'template/head.php'; ?>
 </head>
 <body>
     <div id="wrapper">
-        <?php require_once 'template/contact_header&navbar.php'; ?>
-        <div class="container">
-        </br>
+        <?php require_once 'template/header&navbar.php'; ?>
+        <div class="container" style="margin-top:5px;">
             <?php
-            $count=3;
-                foreach(glob("images/*.*") as $image){
-                    $count+=1;
-                    if($count==4){
-                        $count=0;
-                        echo "
-                                <div class='row'>
-                                    <div class='col-md-3 col-sm-6'>
-                                        <img src='$image' class='thumbnail img-responsive' style='border:1px solid red; width:250px; height:250px;'>
-                                    </div> 
-                             ";
-                    }else{
-                        echo "
-                                <div class='col-md-3 col-sm-6'>
-                                    <img src='$image' class='thumbnail img-responsive' style='border:1px solid red; width:250px; height:250px;'>
-                                </div> 
-                             ";
+            $counter = 0;
+                foreach(glob("images/profilepictures/*.*") as $image){
+                    if($counter === 0) echo "<div class='row'>";
+                    
+                    echo "
+                        <div class='col-md-3 col-sm-6'>
+                            <img src='$image' class='thumbnail img-responsive' style='border:1px solid red; width:250px; height:250px;'>
+                        </div> 
+                         ";
+                    $counter++;
+                    
+                    if($counter === 4) {
+                        echo "</div>";
+                        $counter = 0;
                     }
-                }?>
+                }
+                ?>
         </div>
     </div>
 </body>
