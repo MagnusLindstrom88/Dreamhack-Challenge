41cebdd237b1eb548f464e8b3f9aad80860a2b3c
unknown <robban_rulz@hotmail.com>
2015-06-02 15:35:50 +0200

    fixat faq och about

--- about.php
+++ about.php
@@ -19,8 +19,14 @@
 			font-size: 20px;
 			font-weight: bold;
 		}
-		li {
+		.li1 {
 			margin-left: 11px;
+			color: white;
+			font-style: italic;
+			font-weight: bold;
+		}
+		.p1 {
+			color: white;
 		}
 	</style>
 </head>
@@ -41,15 +47,19 @@
 		<div class="col-md-4">
 		<div class="thumbnail"><img src="images/dh4.jpg" alt="Counter Strike: Global Offensive thumbnail."></div>
 			<h3 style="color: white; ">What is Dreamhack Challenge?</h3>
-				<p style="color: white; ">Dreamhack Challenge is a website where you and your friends can check out the livestream and bet on games for free to win great prizes.
+				<p class="p1"> Dreamhack Challenge is a website where you and your friends can check out the livestream and bet on games for free to win great prizes.
 					There are opportunities to watch various live stream on different games such as:
-					<li style="color: white;"> CS:GO</li>
-					<li style="color: white;"> DOTA2</li>
-					<li style="color: white;"> Starcraft II</li>
-				<p style="color: white; ">We have customized the website for all of you who like Dreamhack and e-sports event.
+					<li class="li1"> CS:GO</li>
+					<li class="li1"> DOTA2</li>
+					<li class="li1"> Starcraft II</li>
+					<br>
+				<p class="p1">We have customized the website for all of you who like Dreamhack and e-sports event.
 					You have opportunities to:
-					<li style="color: white;"><b><i>Create profile</i></b></li>
-					<li style="color: white;"><b><i>Connect with facebook</i></b></li>
+					<li class="li1">Create profile</li>
+					<li class="li1">Connect with facebook</li>
+					<li class="li1">Use our custom made chat with live streams</li>
+					<li class="li1">View your betting history</li>
+					<li class="li1">Take a picture and upload it to your profile</li>
 
 				</p>
 				</p>
@@ -60,12 +70,12 @@
 		<div class="thumbnail"><img src="images/kugg.jpg" alt="Esport"></div>
 
 				<h3 style="color: white; ">How does it work?</h3>
-				<p style="color: white; "> With Dreamhack Challenge there are a few steps that has to de done, then you will be able to start betting and watch livestream.
-					<li style="color: white;"><b><i>Create account</i></b></li>
-					<li style="color: white;"><b><i>Create profile</i></b></li>
+				<p class="p1"> With Dreamhack Challenge there are a few steps that has to de done, then you will be able to start betting and watch livestream.
+					<li class="li1">Create account</li>
+					<li class="li1">Create profile</li>
 
 				</br>
-				<p style="color: white; "> After this steps you will be able to choose which game you like to watch and bet on.
+				<p class="p1"> After this steps you will be able to choose which game you like to watch and bet on.
 					You simply press on the button "Games" and choose the type of game you like to watch, and then you have several of teams you can bet on.
 					Beneath each team there is a bet button which you can press on and your betting will be applied.
 				</p>
@@ -76,12 +86,14 @@
 			<div class="thumbnail"><img src="images/dh3.jpg" alt="#"></div>
 
 			<h3 style="color: white;">Company Info</h3>
-			<p style="color: white;"> We were founded 2015 in Sweden Stockholm, created by students at Stockholm Universitet.
+			<p class="p1"> We were founded 2015 in Sweden Stockholm, created by students at Stockholm Universitet.
 			Our main focus is about Dreamhack and E-sports events, we created a free betting and streaming site for you people to join to watch free livestream and free betting to win fine prizes.
-				<li style="color: white;"><b><i>Created February 2015</i></b></li>
-				<li style="color: white;"><b><i>Lanchued Juny 2015</i></b></li>
+				<li class="li1">Created February 2015</li>
+				<li class="li1">Lanchued Juny 2015</li>
 			</br>
-			<p style="color: white;"> We are constantly developing the website to make it more accessible, more functions for the users.</p>
+			<p class="p1"> We are constantly developing the website to make it more accessible, more functions for the users.</p>
+			<br>
+			<p class="p1">Feel free to contact us at this<a href="contact.php"><button type="button" class="btn btn-primary" style="margin-left: 10px; position: auto;"> page</button></a></p>
 			</div>
 	</div>
 		
--- faq.php
+++ faq.php
@@ -8,6 +8,10 @@
             background-color: #242424;
             border:1px solid #676461;
         }
+        .panel-body{
+            background-color: #242424;
+            border:1px solid #676461;
+        }
     </style>
 </head>
 <body>
@@ -17,44 +21,128 @@
         <div id="content">
             <div class="container">
                 <h1 id="faq-heading">Frequently Asked Questions</h1>
-                
-                <ul id="faq-list" class="list-group">
-                    <li class="list-group-item">
-                        <h4 class="list-group-item-heading">What is Dreamhack Challenge?</h4>
-                        <p class="list-group-item-text">Dreamhack Challenge is a meeting place for anyone who is interested in gaming and e-sports where you can follow the event and interact, compete and bet with others.</p>
-                    </li>
-                    <li class="list-group-item">
-                        <h4 class="list-group-item-heading">How do I login?</h4>
-                        <p class="list-group-item-text">In order to login you have to be registered, you can register either by creating an account on Dreamhack Challenge or by registering with your Facebook account.</p>
-                    </li>
-                    <li class="list-group-item">
-                        <h4 class="list-group-item-heading">How does the betting work?</h4>
-                        <p class="list-group-item-text">When signed in you simply click on the game that your interested in, on the page of each respective supported game the matches to be played will appear. Simply click on the bet-button on the team you believe is going to win to place your bet.</p>
-                    </li>
-                    <li class="list-group-item">
-                        <h4 class="list-group-item-heading">What are your supported games?</h4>
-                        <p class="list-group-item-text">The three biggest ones on the Dreamhack event, that is Dota 2, Counterstrike:Global offensive and Starcraft 2.</p>
-                    </li>
+                <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
                     
-                    <li class="list-group-item">
-                        <h4 class="list-group-item-heading">I am under 18, can I still bet?</h4>
-                        <p class="list-group-item-text">Yes! Since there is no money involved in the betting you can be under 18 and still bet.</p>
-                    </li>
-                    <li class="list-group-item">
-                        <h4 class="list-group-item-heading">Do you stream the matches?</h4>
-                        <p class="list-group-item-text">Yes we do. Open up the page of a supported game and click on the stream-button on the navbar to open up the stream.</p>
-                    </li>
-                    <li class="list-group-item">
-                        <h4 class="list-group-item-heading">How do I change my user information or profile picture?</h4>
-                        <p class="list-group-item-text">When signed in, you simply click on the my pages-button on the navbar to edit your personal information.</p>
-                    </li>
-                    <li class="list-group-item">
-                        <h4 class="list-group-item-heading">Is Dreamhack Challenge created by Dreamhack officially?</h4>
-                        <p class="list-group-item-text">No, Dreamhack Challange is an independent meeting place created by students from The Department of Computer and Systems Sciences (DSV) at Stockholm University.</p>
-                    </li>
-                </ul>
-            </div>
-        </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingOne">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          What is Dreamhack Challenge?
+        </a>
+      </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+      <div class="panel-body">
+        Dreamhack Challenge is a meeting place for anyone who is interested in gaming and e-sports where you can follow the event and interact, compete and bet with others.
+      </div>
+    </div>
+  </div>
+  
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingTwo">
+      <h4 class="panel-title">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          How do I login?
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+      <div class="panel-body">
+        In order to login you have to be registered, you can register either by creating an account on Dreamhack Challenge or by registering with your Facebook account.
+      </div>
+    </div>
+  </div>
+  
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingThree">
+      <h4 class="panel-title">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          How does the betting work?
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+      <div class="panel-body">
+        When signed in you simply click on the game that your interested in, on the page of each respective supported game the matches to be played will appear. Simply click on the bet-button on the team you believe is going to win to place your bet.
+      </div>
+    </div>
+  </div>
+
+                
+ <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingFour">
+      <h4 class="panel-title">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+          What are your supported games?
+        </a>
+      </h4>
+    </div>
+    <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+      <div class="panel-body">
+        The three biggest ones on the Dreamhack event, that is Dota 2, Counterstrike:Global offensive and Starcraft 2.
+      </div>
+    </div>
+  </div>
+ 
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingFive">
+      <h4 class="panel-title">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+          I am under 18, can I still bet?
+        </a>
+      </h4>
+    </div>
+    <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+      <div class="panel-body">
+        Yes! Since there is no money involved in the betting you can be under 18 and still bet.
+      </div>
+    </div>
+  </div>
+  
+   <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingSix">
+      <h4 class="panel-title">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+          Do you stream the matches?
+        </a>
+      </h4>
+    </div>
+    <div id="collapseSix" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSix">
+      <div class="panel-body">
+        Yes we do. Open up the page of a supported game and click on the stream-button on the navbar to open up the stream.
+      </div>
+    </div>
+  </div>
+    <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingSeven">
+      <h4 class="panel-title">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+          How do I change my user information or profile picture?
+        </a>
+      </h4>
+    </div>
+    <div id="collapseSeven" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSeven">
+      <div class="panel-body">
+        When signed in, you simply click on the my pages-button on the navbar to edit your personal information.
+      </div>
+    </div>
+  </div>
+    
+     <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingEight">
+      <h4 class="panel-title">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+          Is Dreamhack Challenge created by Dreamhack officially?
+        </a>
+      </h4>
+    </div>
+    <div id="collapseEight" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingEight">
+      <div class="panel-body">
+        No, Dreamhack Challange is an independent meeting place created by students from The Department of Computer and Systems Sciences (DSV) at Stockholm University.
+      </div>
+    </div>
+  </div>
+                </div>
         <?php require_once 'template/footer.php'; ?>
     </div>
 </body>
