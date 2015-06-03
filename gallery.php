c5c875d2986d4355f6c2a458a0dbca1391a0fe16
sebwar <sebwar01@hotmail.com>
2015-06-03 12:18:23 +0200

    Småfix för gallery,php

--- gallery.php
+++ gallery.php
@@ -7,28 +7,31 @@
     <div id="wrapper">
         <?php require_once 'template/header&navbar.php'; ?>
         <div class="container" style="margin-top:5px;">
-            <h4>Here you can browse through the profile pictures of our users.</h4>
-            <?php
-            $counter = 0;
-                foreach(glob("images/profilepictures/*.*") as $image){
-                    if($counter === 0) echo "<div class='row'>";
-                    $username = $db->query("SELECT username FROM users WHERE id=" . explode(".", explode("/", $image)[2])[0])->fetchAll(PDO::FETCH_ASSOC);
-                    
-                    echo "
-                        <div class='col-md-3 col-sm-6'>
-                            <img src='$image' class='thumbnail img-responsive' style='margin-bottom:5px; height:300px; width:300px;' >
-                            <p style='text-align:center;font-weight:bold;'>{$username[0]['username']}</p>
-                        </div> 
-                         ";
-                    $counter++;
-                    
-                    if($counter === 4) {
-                        echo "</div>";
-                        $counter = 0;
+            <div class="content">
+            <h4 style="text-align:center;">Here you can browse through the profile pictures of our users.</h4>
+                <?php
+                $counter = 0;
+                    foreach(glob("images/profilepictures/*.*") as $image){
+                        if($counter === 0) echo "<div class='row'>";
+                        $username = $db->query("SELECT username FROM users WHERE id=" . explode(".", explode("/", $image)[2])[0])->fetchAll(PDO::FETCH_ASSOC);
+                        
+                        echo "
+                            <div class='col-md-3 col-sm-6'>
+                                <img src='$image' class='thumbnail img-responsive' style='margin:0 auto;margin-bottom:5px; height:300px; width:300px;' >
+                                <p style='text-align:center;font-weight:bold;'>{$username[0]['username']}</p>
+                            </div> 
+                             ";
+                        $counter++;
+                        
+                        if($counter === 4) {
+                            echo "</div>";
+                            $counter = 0;
+                        }
                     }
-                }
-            ?>
+                ?>
+            </div>
         </div>
+        <?php require_once 'template/footer.php'; ?>
     </div>
 </body>
 </html>
